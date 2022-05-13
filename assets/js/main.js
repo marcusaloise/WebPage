63ca16d68905aba63321dd0090cc65b7b8c62ac2
marcusaloise <marcusaloise@gmail.com>
2022-05-12 23:15:27 -0400

    Iniciando aba de contato

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,36 @@ var swiperPopular  = new Swiper(".popular__container", {
 });
 
 /*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.value__accordadion-item')
 
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector('.value__accordadion-header')
+
+  accordionHeader.addEventListener('click', () => {
+    const openItem = document.querySelector('.accordion-open')
+
+
+    toggleItem(item)
+
+    if(openItem && openItem!== item) {
+      toggleItem(openItem)
+    }
+      
+  })   
+
+})
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector('.value__accordadion-content')
+
+  if(item.classList.contains('accordion-open')) {
+    accordionContent.removeAttribute('style')
+    item.classList.remove('accordion-open')
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + 'px'
+    item.classList.add('accordion-open')
+  }
+}
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
